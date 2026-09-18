a0e1a1d35f60fe0563c8605781571a415fff8133
Grey <s.v.torovus@gmail.com>
2026-09-18 17:20:17 +0300

    Reset stale connection indicators on process start

--- app/src/main/java/ua/grey/qstarlight/QStarApplication.kt
+++ app/src/main/java/ua/grey/qstarlight/QStarApplication.kt
@@ -2,10 +2,12 @@ package ua.grey.qstarlight
 
 import android.app.Application
 import ua.grey.qstarlight.update.UpdateScheduler
+import ua.grey.qstarlight.ble.BlePrefs
 
 class QStarApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        BlePrefs(this).also { it.ensureDefaults(); it.resetRuntimeLinkStates() }
         PresenceMonitor.ensure(this)
         UpdateScheduler.ensure(this)
     }
