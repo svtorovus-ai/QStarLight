f562405ad337442a3d3d63074fb8b0d9358ef0d6
Grey <s.v.torovus@gmail.com>
2026-09-18 19:44:26 +0300

    Bump QStarLight to 0.5.5 for auto-update test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ua.grey.qstarlight"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.5.4"
+        versionCode = 13
+        versionName = "0.5.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
