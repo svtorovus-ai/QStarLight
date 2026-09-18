89d51b97a77b26ceaad51a7fed90fcc370a56069
Grey <s.v.torovus@gmail.com>
2026-09-18 17:26:44 +0300

    Bump QStarLight to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "ua.grey.qstarlight"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.4.0"
+        versionCode = 8
+        versionName = "0.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     if (hasStableSigning) {
