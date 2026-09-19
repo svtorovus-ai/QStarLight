b475f3e936589448bb0135c8b90db262d0984df5
Grey <s.v.torovus@gmail.com>
2026-09-19 18:16:09 +0300

    Bump QStarLight to 0.5.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ua.grey.qstarlight"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.5.12"
+        versionCode = 21
+        versionName = "0.5.13"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
